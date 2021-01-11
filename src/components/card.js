2f60faa483b85483330651501f2b196f12dfe82b
Ashish Badgujar <67192558+AshishBadgujar@users.noreply.github.com>
2021-01-11 13:23:25 +0530

    Update card.js

--- src/components/card.js
+++ src/components/card.js
@@ -17,7 +17,7 @@ export default function Card({ item }) {
     }
     return (
         <div className="card my-2 shadow" style={{ width: "18rem", height: "auto" }} onClick={() => handleClick(item.imdbID)}>
-            <img src={item.Poster} style={{ height: "14.5rem" }} className="card-img-top" alt="/moviePost.jpg" />
+            <img src={item.Poster} style={{ height: "14.5rem" }} className="card-img-top" alt="./moviePost.jpg" />
             <div className="card-body">
                 <h5 className="card-title m-0">{item.Title}</h5>
             </div>
